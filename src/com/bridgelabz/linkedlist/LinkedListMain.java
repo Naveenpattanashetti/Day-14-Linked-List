b3a3fdecbab457dd2464a59fe66948f6ee21f168
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-12 15:12:38 +0530

    Added :Program for welcome to user

---
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to linkedList DataStructure Program");
+		 
+	}
+}

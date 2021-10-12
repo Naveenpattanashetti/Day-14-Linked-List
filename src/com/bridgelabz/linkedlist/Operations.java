e688f2d6721c5b069a63e75b337321da7e6520a0
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-12 16:03:54 +0530

    Added : Program for the Search the index of 30

--- src/com/bridgelabz/linkedlist/Operations.java
+++ src/com/bridgelabz/linkedlist/Operations.java
@@ -1,14 +1,14 @@
 package com.bridgelabz.linkedlist;
 
 public class Operations {
-	 public static LinkedList addDataAtStart() {
-	        LinkedList linkedList = new LinkedList();
-	        linkedList.push(70);
-	        linkedList.push(30);
-	        linkedList.push(56);
-	        linkedList.print();
-	        return linkedList;
-	    }
+	public static LinkedList addDataAtStart() {
+		LinkedList linkedList = new LinkedList();
+		linkedList.push(70);
+		linkedList.push(30);
+		linkedList.push(56);
+		linkedList.print();
+		return linkedList;
+	}
 
 	public static void addDataAtEnd() {
 		LinkedList linkedList = new LinkedList();
@@ -17,29 +17,36 @@ public class Operations {
 		linkedList.append(70);
 		linkedList.print();
 	}
-	 public static void insertInBetween() {
-	        LinkedList linkedList = new LinkedList();
-	        Node secondNode = linkedList.push(70);
-	        Node firstNode = linkedList.push(56);
-	        Node newNode= new Node(30);
-	        System.out.println("Before:");
-	        linkedList.print();
-	        linkedList.insertInBetween(firstNode, newNode);
-	        System.out.println("after:");
-	        linkedList.print();
 
-	    }
-	  public static void deleteFirstNode() {
-	        LinkedList linkedList = addDataAtStart();
-	        linkedList.pop();
-	        linkedList.print();
-	    }
-	   public static void deleteLastNode() {
-	        LinkedList linkedList = addDataAtStart();
-	        linkedList.popLast();
-	        linkedList.print();
+	public static void insertInBetween() {
+		LinkedList linkedList = new LinkedList();
+		Node secondNode = linkedList.push(70);
+		Node firstNode = linkedList.push(56);
+		Node newNode = new Node(30);
+		System.out.println("Before:");
+		linkedList.print();
+		linkedList.insertInBetween(firstNode, newNode);
+		System.out.println("after:");
+		linkedList.print();
 
+	}
 
-	    }
+	public static void deleteFirstNode() {
+		LinkedList linkedList = addDataAtStart();
+		linkedList.pop();
+		linkedList.print();
+	}
+
+	public static void deleteLastNode() {
+		LinkedList linkedList = addDataAtStart();
+		linkedList.popLast();
+		linkedList.print();
+
+	}
+
+	public static void findNodeBasedOnValue() {
+		LinkedList linkedList = addDataAtStart();
+		linkedList.searchNode(30);
+	}
 
 }

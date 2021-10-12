610fd19e711b0065d7ba76cdb37fd8c74309bf94
Naveen Pattanashetti <pattanashettinaveen.com>
2021-10-12 15:54:27 +0530

    Added : Program for the delete the last node

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -9,7 +9,8 @@ public class LinkedListMain {
 		System.out.println("Press '1' to add the data at the start");
 		System.out.println("Press '2' to add the data at the end");
 		System.out.println("Press '3' to insert data inBetween");
-		 System.out.println("Press '4' to delete data of first node");
+		System.out.println("Press '4' to delete data of first node");
+		System.out.println("Press '5' to delete data of last node");
 		switch (scanner.nextInt()) {
 		case 1:
 			Operations.addDataAtStart();
--- src/com/bridgelabz/linkedlist/Operations.java
+++ src/com/bridgelabz/linkedlist/Operations.java
@@ -34,5 +34,12 @@ public class Operations {
 	        linkedList.pop();
 	        linkedList.print();
 	    }
+	   public static void deleteLastNode() {
+	        LinkedList linkedList = addDataAtStart();
+	        linkedList.popLast();
+	        linkedList.print();
+
+
+	    }
 
 }
